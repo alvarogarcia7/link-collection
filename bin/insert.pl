586ff25ee6d214918b4da32333423820f72115c9
Alvaro Garcia <derecerca@gmail.com>
2018-06-27 03:16:43 +0200

    [program] Split tags

--- bin/insert.pl
+++ bin/insert.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 
+use List::Flatten;
+
 use strict;
 use warnings;
 
@@ -39,9 +41,31 @@ my $link =  read_one_line("Input link (1 line, optional)\n");
 
 my $body = join('\n', read_until_ctrl_d("Input body, as is. Ctrl+D to finish inputting\n"));
 
+##Fake
+#my $title = "title";
+#my $link =  "link"; 
+#my $body = "body";
+
 ## Read tags (one or more; lowercase, snake-case)
 my @tags = read_until_ctrl_d("Input one tag per line. Ctrl+D to finish inputting\n"); 
-my @tags2 = map {my $tag=$_; $tag=lc($tag); $tag =~ s/(\w) /$1-/g; $tag} map {my $tag=$_; chomp $tag; $tag =~ s/^\s*//; $tag} map { s/Tags://i; $_ } map { $_ } @tags;
+my @tags2 = 
+  map {my $tag=$_; chomp $tag; $tag =~ s/^\s*//; $tag} # trim both sides
+  flat
+  map { my @tags=split(/,/, $_); @tags } #split by comma
+  map {my $tag=$_; $tag =~ s/(\w) /$1-/g; $tag} # snake-case
+  map {my $tag=$_; $tag=lc($tag); $tag} # lowercase
+  map {my $tag=$_; chomp $tag; $tag =~ s/^\s*//; $tag} # trim both sides
+  map { s/Tags://i; $_ } 
+  map { $_ } 
+  @tags;
+#my @tags2 = @tags
+#    .map { $_ }
+#    .map { s/Tags://i }
+#    .map { my $tag=$_; chomp $tag; $tag =~ s/^\s*//; $tag}
+#    .map { my $tag=$_; $tag=lc($tag); $tag =~ s/(\w) /$1-/g; $tag} 
+#    .map { my @tags=split(/,/, $_); @tags }
+#    .flat;
+
 print (map {"$_\n"} @tags2);
 my @tagsAsFields = map {"-f Tag -v \"$_\""} @tags2;
 
